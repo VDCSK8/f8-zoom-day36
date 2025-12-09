fe8db80c293edc94254156d3e38e34801a821887
VIEN DINH CUONG <cuongvien13@gmail.com>
2025-12-09 23:56:59 +0700

    Hoàn thiện bài 1

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
 export default function Header() {
   return (
-    <header style={{ padding: "20px", background: "#ddd" }}>
-      <h2>Header Chung</h2>
+    <header className={styles.header}>
+      <nav className={styles.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/posts">Posts</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/privacy">Privacy</Link>
+
+        {/* Auth */}
+        <Link to="/login">Login</Link>
+
+        {/* Admin */}
+        <Link to="/admin">Admin</Link>
+      </nav>
     </header>
   );
 }
